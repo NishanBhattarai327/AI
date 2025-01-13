35f458e28cd2d6b11017e7842a58d17a40c4c1e0
NishanBhattarai327 <bhattarainishan327@gmail.com>
2025-01-13 07:04:45 +0545

    upto q20

--- lab1/q19.py
+++ lab1/q19.py
@@ -2,3 +2,42 @@
 # methods start() and stop(). The Car class should have instances of Engine and Wheel classes
 # as attributes. Implement a method start_car() in the Car class which starts the engine and prints
 # "Car started".
+
+class Engine:
+    def __init__(self, type):
+        self.type = type
+
+    def start(self):
+        print(f"{self.type} Engine started")
+
+    def stop():
+        print("Engine stoped")
+
+class Wheel:
+    def __init__(self, type):
+        self.type = type
+
+    def start(self):
+        print(f"{self.type} Wheel started")
+
+    def stop():
+        print("Wheel stoped")
+
+class Car:
+    def __init__(self, engine, wheel):
+        self.engine = engine
+        self.wheel = wheel
+    
+    def start_car(self):
+        self.engine.start()
+        print("Car started")
+
+
+def main():
+    e = Engine("Disel")
+    w = Wheel("MRF")
+    c = Car(e, w)
+    c.start_car()
+
+if __name__ == "__main__":
+    main()
--- lab1/q20.py
+++ lab1/q20.py
@@ -1,1 +1,59 @@
 # WAP to represent the following graphs using a dictionary.
+
+Tree = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': ['G'],
+    'E': ['H'],
+    'F': [],
+    'G': [],
+    'H': []
+}
+
+
+G = {
+    'Biratnagar': {
+        'Itahari': 22,
+        'Biratchowk': 30,
+        'Rangeli': 25
+    },
+    'Itahari': {
+        'Biratnagar': 22,
+        'Dharan': 20,
+        'Biratchowk': 11
+    },
+    'Dharan': {
+        'Itahari': 20
+    },
+    'Biratchowk': {
+        'Biratnagar': 30,
+        'Itahari': 11,
+        'Kanepokhari': 10
+    },
+    'Rangeli': {
+        'Biratnagar': 25,
+        'Kanepokhari': 25,
+        'Urlabari': 40
+    },
+    'Kanepokhari': {
+        'Biratchowk': 10,
+        'Rangeli': 25,
+        'Urlabari': 12
+    },
+    'Urlabari': {
+        'Rangeli': 40,
+        'Kanepokhari': 12,
+        'Damak': 6
+    },
+    'Damak': {
+        'Urlabari': 6
+    },
+}
+
+def main():
+    print(f'a) tree = {Tree}')
+    print(f'b) graph = {G}')
+
+if __name__ == "__main__":
+    main()
